c2666062e47111385b84da5c5f8ba3f14fdaedd5
shivkant.d <shivkant.d@osmosys.asia>
2022-04-18 13:46:37 +0530

    configure store

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import products from "./data/products.js";
 import connectDB from "./config/db.js";
-import colors from 'colors';
+import colors from "colors";
 import dotenv from "dotenv";
+import productRoutes from "./routes/ProductRouter.js";
 
 dotenv.config();
 
@@ -13,15 +14,29 @@ const app = express();
 app.get("/", (req, res) => {
 	res.send("API is running...");
 });
-app.get("/api/products", (req, res) => {
-	res.json(products);
+
+//This routing handler is above the middleware to avoid collision with the below middleware
+app.use("/api/products", productRoutes); //Routing level middleware
+
+app.use((req,res,next)=>{
+    const error=new Error(`Not Found - ${req.originalUrl}`);
+	res.status(404);
+	next(error);	
+})
+
+app.use((err, req, res, next) => {
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+	res.status(statusCode);
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === "production" ? null : err.stack,
+	});
 });
-app.get("/api/products/:id", (req, res) => {
-	const product = products.find((p) => p._id === req.params.id);
-	res.json(product);
-});
+
 const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+	)
 );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,14 @@ import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
-root.render(<App />);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
